040d948df4ed61320603acda35547066cbaef5e3
MonstaCat <andikarizkipradanaa@gmail.com>
2021-04-25 16:01:59 +0700

    particles js integration

---
+++ app.js
@@ -0,0 +1,7 @@
+var imported = document.createElement('script');
+imported.src = 'assets/particles/particles.min.js';
+document.head.appendChild(imported);
+
+var imported = document.createElement('script');
+imported.src = 'assets/particles/particles-config.min.js';
+document.head.appendChild(imported);
