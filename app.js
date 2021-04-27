15e910d24361645f31016e15e59f2e08e50cd482
Andika Rizki Pradana <andikarizki19ti@mahasiswa.pcr.ac.id>
2021-04-27 21:23:43 +0700

    Cleaning dir

--- app.js
+++
@@ -1,7 +0,0 @@
-var imported = document.createElement('script');
-imported.src = 'assets/particles/particles.min.js';
-document.head.appendChild(imported);
-
-var imported = document.createElement('script');
-imported.src = 'assets/particles/particles-config.min.js';
-document.head.appendChild(imported);
