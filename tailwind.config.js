a85154af6c6496c27dffaa16b4367404d55de897
monstacat <andikarizkipradanaa@gmail.com>
2021-04-29 02:46:21 +0700

    tailwind configuration and adding assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'ihsao-primary': '#252730',
+        'ihsao-yellow': '#deae42',
+        'ihsao-green': '#61af93',
+        'ihsao-purple': '#6761a9',
+        'ihsao-pink': '#e1788e'
+      }),
+      textColor: theme => theme('colors'),
+      textColor: {
+        'ihsao-primary': '#252730',
+        'ihsao-yellow': '#deae42',
+        'ihsao-green': '#61af93',
+        'ihsao-purple': '#6761a9',
+        'ihsao-pink': '#e1788e'
+      }
+    },
   },
   variants: {
     extend: {},
