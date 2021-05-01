d84d908c83ff4d8a583cf0673ba57df4c6996678
monstacat <andikarizkipradanaa@gmail.com>
2021-05-01 22:19:30 +0700

    changing favicon

--- panitia/templates/header.php
+++ panitia/templates/header.php
@@ -5,12 +5,18 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
-    <link rel="shortcut icon" href="assets/img/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png" />
+    <title>IHSAO 21 - <?= isset($PageTitle) ? $PageTitle : "Default Title" ?></title>
+
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../src/public/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../src/public/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../src/public/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/src/public/img/favicon/site.webmanifest">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" />
     <link rel="stylesheet" href="assets/styles/tailwind.css" />
-    <title>IHSAO 21 - <?= isset($PageTitle) ? $PageTitle : "Default Title" ?></title>
 
     <!-- Additional tags here -->
     <?php if (function_exists('customPageHeader')) {
