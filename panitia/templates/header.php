8aab16f3bd0c0ecbbbbd3d79e107499f1c8cc966
monstacat <andikarizkipradanaa@gmail.com>
2021-05-02 19:29:46 +0700

    adding select2js

--- panitia/templates/header.php
+++ panitia/templates/header.php
@@ -24,6 +24,9 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="assets/styles/datatables-custom.css">
 
+    <!-- Select 2 Js -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="assets/styles/custom.css">
 
