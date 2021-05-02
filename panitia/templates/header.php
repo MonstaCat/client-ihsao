e0e36d9b4029aa5a5f9c8a6f54d1d9f4c63416c9
monstacat <andikarizkipradanaa@gmail.com>
2021-05-02 15:42:53 +0700

    adding datatables library

--- panitia/templates/header.php
+++ panitia/templates/header.php
@@ -7,19 +7,24 @@
     <meta name="theme-color" content="#000000" />
     <title>IHSAO 21 - <?= isset($PageTitle) ? $PageTitle : "Default Title" ?></title>
 
-
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="../src/public/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../src/public/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../src/public/img/favicon/favicon-16x16.png">
     <link rel="manifest" href="/src/public/img/favicon/site.webmanifest">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" />
     <link rel="stylesheet" href="assets/styles/tailwind.css" />
 
-    <!-- Additional tags here -->
-    <?php if (function_exists('customPageHeader')) {
-        customPageHeader();
-    } ?>
+    <!--Regular Datatables CSS-->
+    <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+    <!--Responsive Extension Datatables CSS-->
+    <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
+    <!--Button Extension Datatables CSS-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="assets/styles/datatables-custom.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/styles/custom.css">
+
 </head>
