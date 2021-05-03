06664288eb3cc0c4075a35749d005513f298d4fb
monstacat <andikarizkipradanaa@gmail.com>
2021-05-03 16:06:37 +0700

    adding countdown js

--- index.php
+++ index.php
@@ -12,36 +12,111 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/src/public/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/src/public/img/favicon/favicon-16x16.png">
     <link rel="manifest" href="/src/public/img/favicon/site.webmanifest">
-
-    <link rel="stylesheet" href="/src/public/particles/particles.css">
+    <!-- Particles Js -->
+    <link rel="stylesheet" href="/src/public/css/particles.css">
+    <!-- Tailwind Css -->
     <link rel="stylesheet" href="/src/public/css/tailwind.css">
+    <!-- Countdown Css -->
+    <link rel="stylesheet" href="/src/public/css/jquery.countdown.css">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="/src/public/css/custom.css">
 </head>
 
 <body>
     <div class="flex flex-col h-screen">
 
-        <header class="py-3 text-white text-base bg-ihsao-primary">
-            <div class=" container mx-auto flex justify-between">
+        <header class="fixed w-full text-white text-base bg-transparent z-50" id="nav">
+            <div class="container mx-auto flex justify-between">
                 <div class="flex left-0">
                     <a href="" class="flex items-center">
-                        <div class="block font-extrabold text-2xl tracking-widest text-ihsao-green">
-                            <p>IHSAO</p>
-                            <p>2021</p>
+                        <div class="text-3xl header-text tracking-widest" id="header-title">
+                            <p class="ml-1 -m-2">IHSAO</p>
+                            <p class="ml-1 -m-2">2021</p>
                         </div>
-                        <img class="h-16 m-1" src="/src/public/img/ihsao png.png" alt="">
+                        <img class="h-14 ml-3" src="/src/public/img/ihsao png.png" alt="" id="header-icon">
                     </a>
                 </div>
                 <div class="flex right-0">
-                    <a href="" class="ml-14 flex items-center">Beranda</a>
-                    <a href="" class="ml-14 flex items-center">Tentang IHSAO</a>
-                    <a href="" class="ml-14 flex items-center">Kategori Olimpiade</a>
-                    <a href="" class="ml-14 flex items-center">Timeline</a>
+                    <a href="#sec-1" class="uppercase ml-10 flex items-center font-thin font-mono tracking-wide" id="section-1">Beranda</a>
+                    <a href="#sec-2" class="uppercase ml-10 flex items-center font-thin font-mono tracking-wide" id="section-2">Tentang IHSAO</a>
+                    <a href="#sec-3" class="uppercase ml-10 flex items-center font-thin font-mono tracking-wide" id="section-3">Kategori Olimpiade</a>
+                    <a href="#sec-4" class="uppercase ml-10 flex items-center font-thin font-mono tracking-wide">Timeline</a>
                     <a href="" class="ml-14 mt-4 mb-4 flex items-center w-32 h-10 justify-center font-mono tracking-widest bg-ihsao-yellow text-black border-2 border-black box-shadow-black">Masuk!</a>
                 </div>
+            </div>
         </header>
 
-        <main class="mb-auto" id="particles-js">
-        </main>
+        <div id="particles-js">
+            <div id="inside">
+                <ul id="event-countdown">
+                    <li><span class="days text-white countdown-text">00</span>
+                        <p class=" days_text text-white uppercase font-thin font-mono tracking-widest">Days</p>
+                    </li>
+                    <li class="seperator text-white countdown-text">:</li>
+                    <li><span class="hours text-white countdown-text">00</span>
+                        <p class="hours_text text-white uppercase font-thin font-mono tracking-widest">Hours</p>
+                    </li>
+                    <li class="seperator text-white countdown-text">:</li>
+                    <li><span class="minutes text-white countdown-text">00</span>
+                        <p class="minutes_text text-white uppercase font-thin font-mono tracking-widest">Minutes</p>
+                    </li>
+                    <li class="seperator text-white countdown-text">:</li>
+                    <li><span class="seconds text-white countdown-text">00</span>
+                        <p class="seconds_text text-white uppercase font-thin font-mono tracking-widest">Seconds</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <section class="h-auto bg-green-700" id="sec-2">
+            <div class="container">
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+            </div>
+        </section>
+
+        <section class="h-auto mb-auto bg-red-600" id="sec-3">
+            <div class="container">
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+                <h1>Aaaa</h1><br>
+            </div>
+        </section>
 
         <footer class="flex text-sm font-bold font-mono items-center justify-between bg-yellow-500 border-t-2 h-7 border-black">
             <div class="flex ml-5">
@@ -54,9 +129,61 @@
 
     </div>
 
+    <!-- Jquery Js -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="/src/public/particles/particles.min.js"></script>
-    <script src="/src/public/particles/particles-config.min.js"></script>
+    <!-- Countdown Js -->
+    <script src="/src/public/js/jquery.countdown.min.js"></script>
+    <!-- Particles Js -->
+    <script src="/src/public/js/particles.min.js"></script>
+    <script src="/src/public/js/particles-config.min.js"></script>
+    <script>
+        // initialize countdown
+        var now = new Date();
+        var day = now.getDate();
+        var month = now.getMonth() + 1;
+        var year = now.getFullYear() + 1;
+
+        $('#event-countdown').countdown({
+            date: '07/10/2021 00:00:00', // TODO Date format: 07/27/2017 17:00:00
+            offset: +7, // TODO Your Timezone Offset
+            day: 'Day',
+            days: 'Days',
+            hideOnComplete: true
+        }, function(container) {
+            alert('Done!');
+        });
+
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            // add some class if scroll
+            if (scroll >= 50) {
+                $("#nav").addClass("bg-ihsao-primary");
+                $("#nav").addClass("shadow-2xl");
+                $("#nav").addClass("transition-all");
+
+                $("#header-title").removeClass("text-3xl")
+                $("#header-title").addClass("text-2xl")
+                $("#header-title").addClass("transition-all")
+
+                $("#header-icon").removeClass("h-14");
+                $("#header-icon").addClass("h-12");
+                $("#header-icon").addClass("transition-all")
+            } else {
+                $("#header-title").addClass("text-3xl");
+                $("#header-icon").addClass("h-14");
+                $("#nav").removeClass("bg-ihsao-primary");
+                $("#nav").removeClass("shadow-2xl");
+            }
+
+            // add active class when clicked
+            $("a").click(function() {
+                $(this).siblings().removeClass("active");
+                $(this).addClass("active");
+            });
+
+        });
+    </script>
 </body>
 
 </html>
