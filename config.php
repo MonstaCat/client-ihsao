258a995e0872e6bcd904fbb4d2158def10f08c0e
monstacat <andikarizkipradanaa@gmail.com>
2021-05-30 00:45:16 +0700

    adding config page for dev testing

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// If using localhost with xampp
+// define('BASE_URL', 'http://localhost/client-ihsao/');
+
+// If using vscode serve php extension
+define('BASE_URL', 'http://localhost:3000/');
+
+// Mobile testing
+// define('BASE_URL', 'http://192.168.1.6/client-ihsao/');
