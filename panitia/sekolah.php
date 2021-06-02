b9622e3628598019658ef10ac3db3e9768c39624
ragamuliakusuma <ragamuliaa@gmail.com>
2021-06-02 14:28:19 +0700

    integrating client side with API

--- panitia/sekolah.php
+++ panitia/sekolah.php
@@ -30,19 +30,21 @@ include_once('templates/header.php');
                                         </h2>
 
                                         <!-- Content Body -->
-                                        <div class="flex flex-col">
-                                            <div class="md:flex">
-                                                <div class="md:w-full mr-3 mb-6 md:mb-0 flex-1">
-                                                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="sekolah">
-                                                        Nama Sekolah
-                                                    </label>
-                                                    <input type="text" placeholder="Nama Sekolah" id="sekolah" name="sekolah" autocomplete="off" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2">
+                                        <form id="form-sekolah">
+                                            <div class="flex flex-col">
+                                                <div class="md:flex">
+                                                    <div class="md:w-full mr-3 mb-6 md:mb-0 flex-1">
+                                                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="sekolah">
+                                                            Nama Sekolah
+                                                        </label>
+                                                        <input type="text" placeholder="Nama Sekolah" id="sekolah" name="sekolah" autocomplete="off" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2">
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <button type="submit" class="float-right bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
-                                            Submit Data Sekolah
-                                        </button>
+                                            <button type="submit" class="float-right bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                                                Submit Data Sekolah
+                                            </button>
+                                        </form>
                                         <!-- End of content body -->
                                     </div>
                                 </div>
@@ -58,7 +60,49 @@ include_once('templates/header.php');
 
     <!-- jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="../api-routing.js"></script>
+    <script type="text/javascript">
 
+        // form untuk menambahkan sekolah baru
+        $( `#form-sekolah` ).submit( function( e ){
+            e.preventDefault();
+
+            $( `button[ type="submit" ]` ).text( `Loading...` );
+
+            let serializeData = $(this).serializeArray() 
+            let formData = {};
+
+            for( let i = 0; i < serializeData.length; i++ ){
+                const data = { [ serializeData[ i ].name ]: serializeData[ i ].value };
+
+                formData = { ...formData, ...data };
+            }
+
+
+            formData = JSON.stringify( formData )
+   
+            fetch( API_SEKOLAH, {
+                method: `POST`,
+                mode: `cors`,
+                headers: {
+                    "Authorization": API_KEY,
+                    "Content-Type": "application/json"
+                },
+                body: formData
+            } )
+            .then( response => response.json() )
+            .then( result => {
+                $( `button[ type="submit" ]` ).text( `Submit Data Sekolah` );
+
+                alert( result.msg );
+
+                if( result.code == 200 ) {
+                    $( `input[ name="sekolah" ] ` ).val("");
+                    $( `input[ name="sekolah" ] ` ).focus();
+                }
+            } )
+        } )
+    </script>
 </body>
 
 </html>
