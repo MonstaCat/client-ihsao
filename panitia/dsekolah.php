e71ee2a44f6e10895725f247e4806965b8c5ffaf
ragamuliakusuma <ragamuliaa@gmail.com>
2021-06-05 17:58:53 +0700

    render data & integrate delete function

--- panitia/dsekolah.php
+++ panitia/dsekolah.php
@@ -32,40 +32,14 @@ include_once('templates/header.php');
                                         <table id="data-sekolah">
                                             <thead>
                                                 <tr>
-                                                    <th data-priority="1">ID</th>
+                                                    <!-- <th data-priority="1">ID</th> -->
                                                     <th data-priority="2">Nama Sekolah</th>
                                                     <th data-priority="3">Action</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>SEK001</td>
-                                                    <td>Sekolah A</td>
-                                                    <td>
-                                                        <button class="bg-yellow-500 text-white active:bg-yellow-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="isDialogOpen = true">
-                                                            Edit
-                                                        </button>
-                                                        <button class=" bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                                                            Hapus
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>SEK002</td>
-                                                    <td>Sekolah B</td>
-                                                    <td>
-                                                        <button class="bg-yellow-500 text-white active:bg-yellow-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="isDialogOpen = true">
-                                                            Edit
-                                                        </button>
-                                                        <button class="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                                                            Hapus
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
                                             <tfoot>
                                                 <tr>
-                                                    <th data-priority="1">ID</th>
+                                                    <!-- <th data-priority="1">ID</th> -->
                                                     <th data-priority="2">Nama Sekolah</th>
                                                     <th data-priority="3">Action</th>
                                                 </tr>
@@ -91,15 +65,77 @@ include_once('templates/header.php');
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.22/b-1.6.4/b-flash-1.6.4/b-html5-1.6.4/b-print-1.6.4/datatables.min.js"></script>
 
+    <script src="../api-routing.js"></script>
+
     <script>
         $(document).ready(function() {
-            let table = $('#data-sekolah').DataTable({
+            /**
+             * Integrasikan datatable ke table sekolah
+             */
+            var table = $('#data-sekolah').DataTable({
                 responsive: true,
                 dom: 'Blfrtip',
+                serverSide: true,
                 buttons: [
                     'copy', 'excel', 'pdf'
+                ],
+                ajax: {
+                    url: "http-request/datatable.data-sekolah.php",
+                    dataType: "JSON"
+                },
+                columnDefs: [
+                    {
+                        "render": ( data, type, row ) => {
+                            return row[ 1 ] 
+                        },
+                        "targets": 0
+                    },
+                    {
+                        "render": ( data, type, row ) => {
+                            const element = `<button token="${row[ 0 ]}" class="button-edit bg-yellow-500 text-white active:bg-yellow-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="isDialogOpen = true">
+                                                Edit
+                                            </button>
+                                            <button token="${row[ 0 ]}"  class=" button-hapus bg-red-500 text-white active:bg-red-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                                                Hapus
+                                            </button>`;
+                            return element;
+                        },
+                        targets: 1
+                    }
                 ]
-            }).columns.adjust().responsive.recalc();
+            })
+
+            /**
+             * membuat event hapus sekolah
+             */
+            $( document ).on( "click", `.button-hapus`, function( e ){
+                var id = $(this).attr( `token` );
+                var isDelete = confirm( `Yakin ingin menhapus data ini ?` );
+
+                if( isDelete ) {
+                    const parent = $( this ).closest( "tr" );
+                    const formData = JSON.stringify( { id: id } );
+
+                    $( this ).text( `Loading...` );
+
+                    fetch( API_SEKOLAH, {
+                        method: "delete",
+                        mode: "cors",
+                        headers: {
+                            "Authorization": API_KEY,
+                            "Content-Type": "application/json"
+                        },
+                        body: formData
+                    } )
+                    .then( response => response.json() )
+                    .then( result => {
+                        parent.remove();
+                        table.ajax.reload();
+
+                        alert( result.msg );
+                    } )
+                }
+            } )
         });
     </script>
 </body>
