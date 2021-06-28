0f3fcf618f93c7876efa1a42d5d75dc401115c55
ragamuliakusuma <ragamuliaa@gmail.com>
2021-06-28 14:04:47 +0700

    integrate client to API

--- panitia/soal/soal.php
+++ panitia/soal/soal.php
@@ -32,7 +32,7 @@ include_once('../templates/header.php');
                                         </h2>
 
                                         <!-- Content Body -->
-                                        <form name="form-soal">
+                                        <form id="form-soal">
                                             
                                         
                                         <div class="flex flex-col">
@@ -45,7 +45,7 @@ include_once('../templates/header.php');
                                                     <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mt-4 mb-2" for="soal">
                                                         Input Gambar
                                                     </label>
-                                                    <input type="file" name="gambar-soal" accept="image/*">
+                                                    <input type="file" name="soal-gambar" accept="image/*">
                                                 </div>
                                             </div>
                                             <div class="md:flex">
@@ -54,34 +54,34 @@ include_once('../templates/header.php');
                                                         Centang Jawaban Benar
                                                     </label>
                                                     <div class="flex items-center mb-3">
-                                                        <input type="checkbox" id="jawaban1" name="jawaban1" value="">
-                                                        <input type=" text" placeholder="Jawaban 1" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
-                                                        <input type="file" name="gambar-jawaban-1" accept="image/*">
+                                                        <input type="checkbox" id="jawaban1" name="jawaban1-checkbox" value="a">
+                                                        <input type=" text" placeholder="Jawaban 1" name="jawaban1-text" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
+                                                        <input type="file" name="jawaban1-gambar" accept="image/*">
                                                     </div>
                                                     <div class="flex items-center mb-3">
-                                                        <input type="checkbox" id="jawaban2" name="jawaban2" value="">
-                                                        <input type=" text" placeholder="Jawaban 2" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
-                                                        <input type="file" name="gambar-jawaban-2" accept="image/*">
+                                                        <input type="checkbox" id="jawaban2" name="jawaban2-checkbox" value="b">
+                                                        <input type=" text" placeholder="Jawaban 2" name="jawaban2-text" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
+                                                        <input type="file" name="jawaban2-gambar" accept="image/*">
                                                     </div>
                                                     <div class="flex items-center mb-3">
-                                                        <input type="checkbox" id="jawaban3" name="jawaban3" value="">
-                                                        <input type=" text" placeholder="Jawaban 3" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
-                                                        <input type="file" name="gambar-jawaban-3" accept="image/*">
+                                                        <input type="checkbox" id="jawaban3" name="jawaban3-checkbox" value="c">
+                                                        <input type=" text" placeholder="Jawaban 3" name="jawaban3-text" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
+                                                        <input type="file" name="jawaban3-gambar" accept="image/*">
                                                     </div>
                                                     <div class="flex items-center mb-3">
-                                                        <input type="checkbox" id="jawaban4" name="jawaban4" value="">
-                                                        <input type=" text" placeholder="Jawaban 4" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
-                                                        <input type="file" name="gambar-jawaban-4" accept="image/*">
+                                                        <input type="checkbox" id="jawaban4" name="jawaban4-checkbox" value="d">
+                                                        <input type=" text" placeholder="Jawaban 4" name="jawaban4-text" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
+                                                        <input type="file" name="jawaban4-gambar" accept="image/*">
                                                     </div>
                                                     <div class="flex items-center mb-3">
-                                                        <input type="checkbox" id="jawaban5" name="jawaban5" value="">
-                                                        <input type=" text" placeholder="Jawaban 5" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
-                                                        <input type="file" name="gambar-jawaban-5" accept="image/*">
+                                                        <input type="checkbox" id="jawaban5" name="jawaban5-checkbox" value="e">
+                                                        <input type=" text" placeholder="Jawaban 5" name="jawaban5-text" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-1 px-2 mr-2" style="margin-left: 10px;">
+                                                        <input type="file" name="jawaban5-gambar" accept="image/*">
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <button type="submit" class="float-right bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                                        <button name="btn-tambah" type="submit" class="float-right bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                                             Submit Data Soal
                                         </button>
                                         </form>
@@ -103,8 +103,63 @@ include_once('../templates/header.php');
 
     <!-- jQuery-TE -->
     <script type="text/javascript" src="<?= BASE_URL ?>/src/public/plugin/jQuery-TE/jquery-te-1.4.0.min.js"></script>
+    <script type="text/javascript" src="<?= BASE_URL ?>/api-routing.js"></script>
     <script>
         $("textarea").jqte();
+
+        /**
+         * Ketika form soal di submit
+         */
+        $(`form#form-soal`).submit( function( e ){
+            e.preventDefault();
+
+            const _this = $(this);
+            const formData = new FormData();
+            const MAX_JAWABAN = 5;
+            const button = $(`[name="btn-tambah"]`);
+
+            let body = {}  // penyimpanan form field
+
+            // tambahkan data soal
+            formData.append( `soal-gambar`, _this.find( `[name="soal-gambar"]` )[0].files[0] );
+
+            const dataSoal = {
+                text: _this.find( `[name="pertanyaan"]` ).val(),
+                image: formData.get( `soal-gambar` )
+            }
+
+            // tambahkan data jawaban
+            let jawaban = {};
+            let jawabanCollection = [];
+            let jawabanKey = "a";
+
+            for( let i = 1; i <= MAX_JAWABAN; i++ ) {
+                formData.append( `jawaban${i}-gambar`, _this.find( `[name="jawaban${i}-gambar"]` )[0].files[0] );
+
+                const gambar = formData.get( `jawaban${i}-gambar` );
+                const text = _this.find( `[name="jawaban${i}-text"]` );
+                const checkbox = _this.find( `[name="jawaban${i}-checkbox"]` );
+
+                // mengatur kunci jawaban
+                if( checkbox.prop( `checked` ) ) {
+                    jawabanKey = checkbox.val();
+                }
+
+                const setJawaban = {
+                    text: {
+                        values: text.val(),
+                        key: checkbox.val()
+                    },
+                    image: gambar
+                }
+
+                jawabanCollection = [ ...jawabanCollection, setJawaban ];
+            }
+
+            jawaban = Object.assign( jawaban, { key: jawabanKey } );
+            jawaban = Object.assign( jawaban, { collection: jawabanCollection } );
+            console.log(jawaban)
+        } )
     </script>
 
 </body>
