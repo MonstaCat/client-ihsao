41ea092153bc3056d549e6e193c8503e8051826f
monstacat <andikarizkipradanaa@gmail.com>
2021-07-11 02:00:47 +0700

    adding assets and templates for peserta

---
+++ peserta/templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>IHSAO 21 - <?= isset($PageTitle) ? $PageTitle : "Default Title" ?></title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo BASE_URL; ?>/src/public/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo BASE_URL; ?>/src/public/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL; ?>/src/public/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo BASE_URL; ?>/src/public/img/favicon/site.webmanifest">
+
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/peserta/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" />
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/peserta/assets/styles/tailwind.css" />
+
+    <!-- Select 2 Js -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    <!-- jQuery-TE -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>/src/public/plugin/jQuery-TE/jquery-te-1.4.0.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/peserta/assets/styles/custom.css">
+
+</head>
