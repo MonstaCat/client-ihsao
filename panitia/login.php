de3e2bc9a0e620bf053eb788ed24e24b1a0d30fb
ragamulikusuma <github.ragamulia@gmail.com>
2021-07-18 18:21:41 +0700

    change origin value

--- panitia/login.php
+++ panitia/login.php
@@ -144,7 +144,7 @@ include('../config.php');
     <script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
     <script src="<?php echo BASE_URL ?>/api-routing.js"></script>
     <script>
-        const socket = io(`http://localhost:3000`);
+        const socket = io(API_ORIGIN);
         // const API_PESERTA = `http://localhost:3000/api/peserta`;
 
         /**
