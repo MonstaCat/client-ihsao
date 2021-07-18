edf706e7ba68f74396b883474ec33f524a11275f
ragamulikusuma <github.ragamulia@gmail.com>
2021-07-18 18:11:58 +0700

    add dynamic api key

--- api-routing.js
+++ api-routing.js
@@ -1,5 +1,6 @@
 const BASE_URL			= "http://localhost/ihsao";
 const BASE_API 			= "http://localhost:3000/api";
+var API_ADMIN 			= `${BASE_API}/admin`;
 var API_PESERTA 		= `${BASE_API}/peserta`;
 var API_ADMIN 			= `${BASE_API}/admin`;
 var API_LOMBA 			= `${BASE_API}/lomba`;
@@ -7,4 +8,4 @@ var API_TIM 			= `${BASE_API}/tim`;
 var API_SEKOLAH 		= `${BASE_API}/sekolah`;
 var API_SOAL_MULTIPLE 	= `${BASE_API}/soal/multiple`;
 
-var API_KEY = `d033e22ae348aeb5660fc2140aec35850c4da997`;
+var API_KEY = localStorage.getItem( `token` ) ?? null;
