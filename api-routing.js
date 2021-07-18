822fb1904bfa0854a79007fb2217be1cf434e4e0
ragamulikusuma <github.ragamulia@gmail.com>
2021-07-18 18:21:18 +0700

    adding api origin variable

--- api-routing.js
+++ api-routing.js
@@ -1,5 +1,6 @@
 const BASE_URL			= "http://localhost/ihsao";
 const BASE_API 			= "http://localhost:3000/api";
+const API_ORIGIN		= `http://localhost:3000`;
 var API_ADMIN 			= `${BASE_API}/admin`;
 var API_PESERTA 		= `${BASE_API}/peserta`;
 var API_ADMIN 			= `${BASE_API}/admin`;
