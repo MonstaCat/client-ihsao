0e1eef766df226bae3299d6a405aed9eb74058a6
ragamulikusuma <github.ragamulia@gmail.com>
2021-07-19 19:29:44 +0700

    adding banned callback

--- peserta/templates/footer.php
+++ peserta/templates/footer.php
@@ -1,3 +1,19 @@
 <footer>
 
 </footer>
+<script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
+<script src="<?php echo BASE_URL ?>/api-routing.js"></script>
+<script>
+	const socket = io( API_ORIGIN );
+	const token  = localStorage.getItem( `token` );
+	const email  = localStorage.getItem( `email` );
+
+	socket.on( `connect`, () => {
+		socket.emit( `join-to-system`, token );
+		console.log("socket connected");
+	} )	
+
+	socket.on( `banned`, token => {
+		window.location = `${BASE_URL}/peserta/login.php`;
+	} )
+</script>
