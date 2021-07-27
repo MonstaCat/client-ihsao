b840917051c66cb4d4f6ee7ac5e646ff32cd73e5
ragamulikusuma <github.ragamulia@gmail.com>
2021-07-27 14:30:35 +0700

    show user detail

--- peserta/home.php
+++ peserta/home.php
@@ -30,28 +30,28 @@ include_once('templates/header.php');
 										<div class="w-72 mt-5 mb-5 ml-3">
 											<div class="bg-white shadow-lg rounded-lg py-3">
 												<div class="p-2">
-													<h3 class="text-center text-xl text-gray-900 font-medium leading-8">Andika Rizki Pradana</h3>
+													<h3 class="text-center text-xl text-gray-900 font-medium leading-8" id="user-name">Loading...</h3>
 													<div class="text-center text-gray-400 text-xs font-semibold mb-5">
-														<p>Tim Labor A</p>
+														<p id="user-tim">Loading...</p>
 													</div>
 													<table class="text-xs my-3">
 														<tbody>
 															<tr>
 																<td class="px-2 py-2 text-gray-500 font-semibold flex">Asal Sekolah</td>
-																<td class="px-2 py-2">SMKS Labor Binaan FKIP UR Pekanbaru</td>
+																<td class="px-2 py-2" id="user-sekolah">Loading...</td>
 															</tr>
 															<tr>
 																<td class="px-2 py-2 text-gray-500 font-semibold flex">Email</td>
-																<td class="px-2 py-2">andikarizkipradanaa@gmail.com</td>
+																<td class="px-2 py-2" id="user-email">Loading...</td>
 															</tr>
 															<tr>
 																<td class="px-2 py-2 text-gray-500 font-semibold flex">Pembimbing</td>
-																<td class="px-2 py-2">Kurnia Rahman Agus</td>
+																<td class="px-2 py-2" id="user-pembimbing">Loading...</td>
 															</tr>
-															<tr>
+															<!-- <tr>
 																<td class="px-2 py-2 text-gray-500 font-semibold flex">ID</td>
 																<td class="px-2 py-2">P-1955301018</td>
-															</tr>
+															</tr> -->
 														</tbody>
 													</table>
 
@@ -76,8 +76,9 @@ include_once('templates/header.php');
 									<div class="relative w-full max-w-full flex-grow flex-1">
 
 										<!-- Content Body -->
-										<div class="flex">
-											<div class="w-72 mt-5 mb-5 ml-3">
+										<div class="flex" id="tim-list-container">
+											<p>Loading...</p>
+											<!-- <div class="w-72 mt-5 mb-5 ml-3">
 												<div class="bg-white shadow-lg rounded-lg py-3">
 													<div class="p-2">
 														<h3 class="text-center text-xl text-gray-900 font-medium leading-8">Andika Rizki Pradana</h3>
@@ -107,71 +108,7 @@ include_once('templates/header.php');
 
 													</div>
 												</div>
-											</div>
-
-											<div class="w-72 mt-5 mb-5 ml-3">
-												<div class="bg-white shadow-lg rounded-lg py-3">
-													<div class="p-2">
-														<h3 class="text-center text-xl text-gray-900 font-medium leading-8">Andika Rizki Pradana</h3>
-														<div class="text-center text-gray-400 text-xs font-semibold mb-5">
-															<p>Tim Labor A</p>
-														</div>
-														<table class="text-xs my-3">
-															<tbody>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Asal Sekolah</td>
-																	<td class="px-2 py-2">SMKS Labor Binaan FKIP UR Pekanbaru</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Email</td>
-																	<td class="px-2 py-2">andikarizkipradanaa@gmail.com</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Pembimbing</td>
-																	<td class="px-2 py-2">Kurnia Rahman Agus</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">ID</td>
-																	<td class="px-2 py-2">P-1955301018</td>
-																</tr>
-															</tbody>
-														</table>
-
-													</div>
-												</div>
-											</div>
-
-											<div class="w-72 mt-5 mb-5 ml-3">
-												<div class="bg-white shadow-lg rounded-lg py-3">
-													<div class="p-2">
-														<h3 class="text-center text-xl text-gray-900 font-medium leading-8">Andika Rizki Pradana</h3>
-														<div class="text-center text-gray-400 text-xs font-semibold mb-5">
-															<p>Tim Labor A</p>
-														</div>
-														<table class="text-xs my-3">
-															<tbody>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Asal Sekolah</td>
-																	<td class="px-2 py-2">SMKS Labor Binaan FKIP UR Pekanbaru</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Email</td>
-																	<td class="px-2 py-2">andikarizkipradanaa@gmail.com</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">Pembimbing</td>
-																	<td class="px-2 py-2">Kurnia Rahman Agus</td>
-																</tr>
-																<tr>
-																	<td class="px-2 py-2 text-gray-500 font-semibold flex">ID</td>
-																	<td class="px-2 py-2">P-1955301018</td>
-																</tr>
-															</tbody>
-														</table>
-
-													</div>
-												</div>
-											</div>
+											</div> -->
 										</div>
 										<!-- End of content body -->
 									</div>
@@ -188,7 +125,71 @@ include_once('templates/header.php');
 
 	<!-- jQuery -->
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript">
+		async function getUserDetail(endpoint, queryString) {
+			const user = await fetch( `${endpoint}?${queryString}`, {
+				mode   : "cors",
+				method : "get"
+			} )
 
+			return await user.json(); 
+		}
+
+		const UToken = localStorage.getItem("token");
+
+		getUserDetail( API_PESERTA, `token=${UToken}` )
+		.then( response => {
+			const user = response.data[0];
+
+			$( `#user-name` ).text( user.nama );
+			$( `#user-tim` ).text( user.kelompok.nama_tim );
+			$( `#user-sekolah` ).text( user.Sekolah.sekolah );
+			$( `#user-email` ).text( user.email );
+			$( `#user-pembimbing` ).text( user.kelompok.nama_pembina );
+
+			const TId = user.kelompok.id
+
+			getUserDetail( API_PESERTA, `tim=${TId}` )
+			.then( response => {
+				const elContainer = $( `#tim-list-container` );
+
+				elContainer.html("");
+
+				for( let user of response.data ) {
+					const element = `
+					<div class="w-72 mt-5 mb-5 ml-3">
+					<div class="bg-white shadow-lg rounded-lg py-3">
+					<div class="p-2">
+					<h3 class="text-center text-xl text-gray-900 font-medium leading-8">${user.nama}</h3>
+					<div class="text-center text-gray-400 text-xs font-semibold mb-5">
+					<p>${user.kelompok.nama_tim}</p>
+					</div>
+					<table class="text-xs my-3">
+					<tbody>
+					<tr>
+					<td class="px-2 py-2 text-gray-500 font-semibold flex">Asal Sekolah</td>
+					<td class="px-2 py-2">${user.Sekolah.sekolah}</td>
+					</tr>
+					<tr>
+					<td class="px-2 py-2 text-gray-500 font-semibold flex">Email</td>
+					<td class="px-2 py-2">${user.email}</td>
+					</tr>
+					<tr>
+					<td class="px-2 py-2 text-gray-500 font-semibold flex">Pembimbing</td>
+					<td class="px-2 py-2">${user.kelompok.nama_pembina}</td>
+					</tr>
+					</tbody>
+					</table>
+
+					</div>
+					</div>
+					</div>`;
+
+					elContainer.append( element );
+				}
+			} )
+		} )
+	</script>
 </body>
 
 </html>
