746a776e175f12d64e42e40b6ae1534d246bf217
monstacat <andikarizkipradanaa@gmail.com>
2021-08-01 19:54:44 +0700

    Update login UI for peserta & panitia

--- panitia/templates/sidebar.php
+++ panitia/templates/sidebar.php
@@ -3,7 +3,7 @@
         <button class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent" type="button" onclick="toggleNavbar('example-collapse-sidebar')">
             <i class="fas fa-bars"></i>
         </button>
-        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="index.html">
+        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="#">
             Admin Panel IHSAO 2021
         </a>
         <ul class="md:hidden items-center flex flex-wrap list-none">
@@ -27,7 +27,7 @@
             <div class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
                 <div class="flex flex-wrap">
                     <div class="w-6/12">
-                        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="index.html">
+                        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="#">
                             Admin Panel IHSAO 2021
                         </a>
                     </div>
