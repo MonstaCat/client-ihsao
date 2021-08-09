ade429df50c04f8938272ac3d0c9c4a9a4ed2869
monstacat <andikarizkipradanaa@gmail.com>
2021-08-09 15:23:36 +0700

    Deleting generate token menu and files

--- panitia/templates/sidebar.php
+++ panitia/templates/sidebar.php
@@ -157,30 +157,6 @@
             <hr class="my-4 md:min-w-full" />
             <!-- Heading -->
             <h6 class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-                Generate Token
-            </h6>
-
-            <!-- Navigation -->
-            <ul class="md:flex-col md:min-w-full flex flex-col list-none">
-                <li class="items-center">
-                    <a href="<?php echo BASE_URL; ?>/panitia/token/tpeserta.php" class="text-xs uppercase py-3 font-bold block <?= ($PageFile) == "tpeserta" ? "text-pink-500 hover:text-pink-600" : "text-blueGray-700 hover:text-blueGray-500" ?>">
-                        <i class="fas fa-file-upload mr-2 text-sm text-blueGray-300"></i>
-                        Generate Token Peserta
-                    </a>
-                </li>
-
-                <li class="items-center">
-                    <a href="<?php echo BASE_URL; ?>/panitia/token/tpanitia.php" class="text-xs uppercase py-3 font-bold block <?= ($PageFile) == "tpanitia" ? "text-pink-500 hover:text-pink-600" : "text-blueGray-700 hover:text-blueGray-500" ?>">
-                        <i class="fas fa-file-upload mr-2 text-sm text-blueGray-300"></i>
-                        Generate Token Panitia
-                    </a>
-                </li>
-            </ul>
-
-            <!-- Divider -->
-            <hr class="my-4 md:min-w-full" />
-            <!-- Heading -->
-            <h6 class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
                 Fitur Ujian
             </h6>
 
