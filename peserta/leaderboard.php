41a2d133d103e6147ed6c0cb1be99102abb50244
ragamulikusuma <github.ragamulia@gmail.com>
2021-08-10 09:36:53 +0700

    show dynamic leaderboard

--- peserta/leaderboard.php
+++ peserta/leaderboard.php
@@ -53,198 +53,7 @@ include_once('templates/header.php');
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                1
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Tam
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKS Labor Binaan FKIP UR Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>230</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                2
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Bersyukur Kepada Allah
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 1 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-orange-500 mr-2"></i>
-                                            pending
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>220</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                3
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Ibu
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 2 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>210</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                4
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Bapak
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 3 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>200</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                5
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Tante
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 4 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>190</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                6
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Paman
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 5 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>180</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                7
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Sanak
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 6 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>170</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <p class="ml-3 font-bold text-blueGray-600">
-                                                8
-                                            </p>
-                                        </td>
-                                        <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <p class="font-bold text-blueGray-600">
-                                                Tim Do'a Saudara
-                                            </p>
-                                        </th>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            SMKN 7 Pekanbaru
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <i class="fas fa-circle text-emerald-500 mr-2"></i>
-                                            completed
-                                        </td>
-                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div class="flex items-center">
-                                                <p class="mr-2"><b>160</b></p>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    
                                 </tbody>
                             </table>
                         </div>
@@ -262,3 +71,45 @@ include_once('templates/header.php');
 </body>
 
 </html>
+<script type="text/javascript">
+    async function LBDataset() {
+        const token = localStorage.getItem( `token` );
+        const sekolah = localStorage.getItem( `isSmk` );
+        const endpoint = `${API_SOAL_MULTIPLE}/leaderboard/${token}/${sekolah}`;
+
+        const lb = await fetch( endpoint );
+
+        return lb.json();
+    }
+
+    LBDataset()
+    .then( response => {
+       let no = 1;
+       response.data.forEach( v => {
+            $( `tbody` ).append(`<tr>
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+            <p class="ml-3 font-bold text-blueGray-600">
+            ${no}
+            </p>
+            </td>
+            <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+            <p class="font-bold text-blueGray-600">
+            ${v.Tim.nama_tim}
+            </p>
+            </th>
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+            ${v.Sekolah.sekolah}
+            </td>
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+            ${ ( v.is_complete ) ? `<i class="fas fa-circle text-emerald-500 mr-2"></i> Complete` : `<i class="fas fa-circle text-orange-500 mr-2"></i> Pending` }
+            </td>
+            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+            <div class="flex items-center">
+            <p class="mr-2"><b>${v.score}</b></p>
+            </div>
+            </td>
+            </tr>`)
+            no++;
+       } )
+    } )
+</script>
